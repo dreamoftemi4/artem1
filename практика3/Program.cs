d9241ebb34c8c779125f03c5634616c7efa17cb3
dreamoftemi4 <dreamofara@yandex.ru>
2019-11-02 19:05:00 +0300

    second commit

---
+++ практика3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практика3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0, g;
+            for (int v = 0; v <= 3; v++)
+            {
+                x += v;
+                y++;
+            }
+            g = x / y;
+            Console.WriteLine(g);
+            Console.ReadKey();
+        }
+    }
+}
